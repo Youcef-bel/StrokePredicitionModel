a0738abc3117e90773a89252f1115d0e4b458745
Youcef Belmokhtar <78687916+Youcef-bel@users.noreply.github.com>
2024-03-08 15:50:24 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -5,125 +5,100 @@ import streamlit as st
 model = pickle.load(open('clf.sav', 'rb'))
 
 def diabetes_prediction(input_data):
-    
     # changing the input_data to numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 
     # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
     prediction = model.predict(input_data_reshaped)
     print(prediction)
 
-    if (prediction[0] == 0):
-      return 'The person is not diabetic'
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
     else:
-      return 'The person is diabetic'
-        
-    
+        return 'The person is diabetic'
+
 def main():
-    
-    
     # giving a title
     st.title('Stroke Prediction Web App')
-    
-    
+
     # getting the input data from the user
-    
-    
-    gender = st.selectbox('Gender',('Male','Female'))
+    gender = st.selectbox('Gender', ('Male', 'Female'))
     age = st.text_input('Age')
-    hypertension = st.selectbox('Do you have a hypertension ',('Yes','No'))
-    heart_disease = st.selectbox('Do you have a Heart disease ',('Yes','No'))
-    ever_married = st.selectbox('Are you merried? ',('Yes','No'))
-    work_type = st.selectbox('Your work type?',('Private', 'Self-employed', 'Governement job', 'Working with children', 'Never worked'))
-    residence_type = st.selectbox('Residence type ',('Urban','Rural'))
+    hypertension_input = st.selectbox('Do you have hypertension ', ('Yes', 'No'))
+    heart_disease_input = st.selectbox('Do you have a Heart disease ', ('Yes', 'No'))
+    ever_married_input = st.selectbox('Are you married? ', ('Yes', 'No'))
+    work_type_input = st.selectbox('Your work type?', ('Private', 'Self-employed', 'Government job', 'Working with children', 'Never worked'))
+    residence_type_input = st.selectbox('Residence type ', ('Urban', 'Rural'))
     avg_glucose_level = st.text_input('Average glucose level')
     bmi = st.text_input('BMI')
-    smoking_status = st.selectbox('Are you a smoker? ',('Formerly smoked', 'Never smoked', 'I Smoke'))
-    
-    def gender(gender):
-        if gender=='Male':
-            gender=0
-        else:
-            gender=1
-            
-        print(gender)
-        return gender
-        
-    def hypertension(hypertension):
-        if hypertension=='Yes':
-            hypertension=1
-        else:
-            hypertension=0
-        print(hypertension)    
-        return hypertension
+    smoking_status_input = st.selectbox('Are you a smoker? ', ('Formerly smoked', 'Never smoked', 'I Smoke'))
 
-    def heart_disease(heart_disease):
-        if heart_disease=='No':
-            heart_disease=0
-        else:
-            heart_disease=1
+    gender_numeric = convert_gender(gender)
+    hypertension_numeric = convert_hypertension(hypertension_input)
+    heart_disease_numeric = convert_heart_disease(heart_disease_input)
+    ever_married_numeric = convert_ever_married(ever_married_input)
+    work_type_numeric = convert_work_type(work_type_input)
+    residence_type_numeric = convert_residence_type(residence_type_input)
+    smoking_status_numeric = convert_smoking_status(smoking_status_input)
 
-        print(heart_disease)
-        return heart_disease
-        
-    def ever_married(ever_married):
-        if ever_married=='Yes':
-            ever_married=0
-        else:
-            ever_married=1
+    # Create a list of inputs for the prediction function
+    input_data = [gender_numeric, age, hypertension_numeric, heart_disease_numeric, ever_married_numeric, work_type_numeric, residence_type_numeric, avg_glucose_level, bmi, smoking_status_numeric]
 
-        print(ever_married)
-        return ever_married
-    
-    def work_type(work_type):
-        if work_type=='Private':
-            work_type=0
-        elif work_type=='Self-employed':
-            work_type=1
-        elif work_type=='Governement job':
-            work_type=2
-        elif work_type=='Working with children':
-            work_type=3
-        else:
-            work_type=4
-
-        print(work_type)
-        return work_type
-        
-    def residence_type(residence_type):
-        if residence_type=='Urban':
-            residence_type=0
-        else:
-            residence_type=1
-
-        print(residence_type)
-        return residence_type
-        
-    def smoking_status(smoking_status):
-        if smoking_status=='Formerly smoked':
-            smoking_status=0
-        elif smoking_status=='Never smoked':
-            smoking_status=1
-        elif smoking_status=='I Smoke':
-            smoking_status=2 
-
-        print(smoking_status)
-        
-        return smoking_status
-        
     diagnosis = ''
-    
+
     # creating a button for Prediction
     if st.button('Predict!'):
-        diagnosis = diabetes_prediction([gender, age, hypertension, heart_disease, ever_married, work_type, residence_type, avg_glucose_level,bmi,smoking_status])
+        diagnosis = diabetes_prediction(input_data)
     st.success(diagnosis)
-    
-    
-    
-    
-if __name__ == '__main__':
-    main()
-    
-    
+
+def convert_gender(gender):
+    if gender == 'Male':
+        return 0
+    else:
+        return 1
+
+def convert_hypertension(hypertension):
+    if hypertension == 'Yes':
+        return 1
+    else:
+        return 0
+
+def convert_heart_disease(heart_disease):
+    if heart_disease == 'No':
+        return 0
+    else:
+        return 1
+
+def convert_ever_married(ever_married):
+    if ever_married == 'Yes':
+        return 0
+    else:
+        return 1
+
+def convert_work_type(work_type):
+    if work_type == 'Private':
+        return 0
+    elif work_type == 'Self-employed':
+        return 1
+    elif work_type == 'Government job':
+        return 2
+    elif work_type == 'Working with children':
+        return 3
+    else:
+        return 4
+
+def convert_residence_type(residence_type):
+    if residence_type == 'Urban':
+        return 0
+    else:
+        return 1
+
+def convert_smoking_status(smoking_status):
+    if smoking_status == 'Formerly smoked':
+        return 0
+    elif smoking_status == 'Never smoked':
+        return 1
+    elif smoking_status == 'I Smoke':
+        return 2
