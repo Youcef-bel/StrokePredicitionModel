8e8f6887b1cf1b7274a751cb7502f1f4c96c7c3b
Youcef Belmokhtar <78687916+Youcef-bel@users.noreply.github.com>
2024-03-08 16:58:40 +0100

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -71,7 +71,7 @@ def main():
 
 
     st.markdown("---")
-    st.markdown("<h1 style='text-align: left; color: white;'>Introduction</h2>", unsafe_allow_html=True)
+    st.markdown("<h3 style='text-align: left; color: white;'>Introduction</h3>", unsafe_allow_html=True)
     st.write("Our Stroke Prediction Model is an advanced machine learning algorithm designed to assess an individual's likelihood of experiencing a stroke in the future. Stroke, a critical medical condition, requires early detection and preventive measures to minimize the associated health risks.")
     st.markdown("---")
     st.markdown("Developed by Youcef Belmokhtar")
